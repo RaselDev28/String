9006d7b99381a95453696360a35e2f9eefc7c657
Rasel <you@example.com>
2024-10-02 22:21:25 +0600

    case

---
+++ case.js
@@ -0,0 +1,8 @@
+const sub="Physics ";
+const book=' physics';
+if(sub.toLowerCase().trim()===book.toLowerCase().trim()){
+    console.log("I read physics");
+}
+else{
+    console.log("I didnt read book");
+}
